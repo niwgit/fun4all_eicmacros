72851f8910e0d3c7d8bcab334e4131ffc1e1eee0
Nilanga Wickramaarachchi <nwickjlb@ifarm1901.jlab.org>
2022-02-28 13:51:23 -0500

    use parameters for CORE hpDIRC

--- common/G4_DIRC_new.C
+++ common/G4_DIRC_new.C
@@ -44,23 +44,23 @@ void DIRC_newSetup(PHG4Reco *g4Reco)
   G4EicDircSubsystem *eicdirc;
   eicdirc = new G4EicDircSubsystem("DIRC");
 
-  eicdirc->set_double_param("NBars", 10);
-  eicdirc->set_double_param("Radius", 72.96 * cm);
-  eicdirc->set_double_param("Prizm_width", 35.135 * cm);
+  eicdirc->set_double_param("NBars", 5);
+  eicdirc->set_double_param("Radius", 50.17 * cm);
+  eicdirc->set_double_param("Prizm_width", 17.56 * cm);  
   eicdirc->set_double_param("Prizm_length", 30.0 * cm);
   eicdirc->set_double_param("Prizm_height_at_lens", 5.0 * cm);
   eicdirc->set_double_param("Bar_thickness", 1.725 * cm);
   eicdirc->set_double_param("Bar_width", 3.5 * cm);
   eicdirc->set_double_param("BarL_length", 122.5 * cm);
-  eicdirc->set_double_param("BarS_length", 56.0 * cm);
+  eicdirc->set_double_param("BarS_length", 35.0 * cm);
   eicdirc->set_double_param("Mirror_height", 2.0 * cm);
-  eicdirc->set_double_param("z_shift", -43.75 * cm);
-  eicdirc->set_int_param("Geom_type", 0); // 0-whole DIRC, 1-one bar box                                                        
-  eicdirc->set_int_param("Lens_id", 3); // 3- 3-layer spherical lens                                                            
-  eicdirc->set_int_param("MCP_rows", 6);
+  eicdirc->set_double_param("z_shift", 0 * cm);
+  eicdirc->set_int_param("Geom_type", 0); // 0-whole DIRC, 1-one bar box
+  eicdirc->set_int_param("Lens_id", 3); // 3- 3-layer spherical lens
+  eicdirc->set_int_param("MCP_rows", 3);
   eicdirc->set_int_param("MCP_columns", 4);
-  eicdirc->set_int_param("NBoxes", 12); // number of bar boxes                                                                 
-  eicdirc->set_int_param("Bar_pieces", 4); // pieces glued in one bar
+  eicdirc->set_int_param("NBoxes", 16); // number of bar boxes
+  eicdirc->set_int_param("Bar_pieces", 3); // pieces glued in one bar
 
   g4Reco->registerSubsystem(eicdirc); 
 }
